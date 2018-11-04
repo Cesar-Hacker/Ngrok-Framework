9058c6e62b079a613fbd0fda92aedaa226705143
Cesar-Hacker <44127894+Cesar-Hacker@users.noreply.github.com>
2018-11-04 13:59:35 -0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,20 @@
+		clear
+pkg update && pkg upgrade -y
+apt install util-linux -y
+apt install figlet -y
+		clear
+setterm -foreground yellow
+echo "          $green Hecho por Cesar Hacker The Gray"
+setterm -foreground cyan
+figlet      ./Ngrok
+setterm -foreground red
+figlet Cesar Hacker
+	echo "			$green instalando modos"
+chmod 777 $HOME/Ngrok-Framework/ngrok
+cp ngrok /data/data/com.termux/files/usr/bin
+setterm -foreground green
+	echo " 	   $green puede ser usada satisfactoriamente Att.$cyan Cesar Hack"
+setterm -foreground yellow
+	echo "	   $cyan ngrok http 8080,$blue  ngrok tcp 4444,$green  ngrok https 8080" $cyan
+	echo "        $green ./ngrok http 8080, ./ngrok tcp 4444, ./ngrok https 8080"
+
